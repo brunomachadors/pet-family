d4555f77f8ec9ba6cf25c00dc5f4a194a07549cc
brunomachadors <brunomachadors@gmail.com>
2024-08-11 18:58:56 +0100

    Adding new pet icons

--- src/app/assets/imageLinks.ts
+++ src/app/assets/imageLinks.ts
@@ -3,9 +3,35 @@ export const breedImageMap: Record<string, string> = {
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723115596/PET%20FAMILTY/french-bulldog_jbihqs.png',
   Siamês:
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723115596/PET%20FAMILTY/siamese_gpagdt.png',
-  // Adicione mais raças e URLs de imagem conforme necessário
   Viralata:
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723218491/PET%20FAMILTY/viralata.png',
+  'Golden Retriever':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/goldenretriever.png',
+  'Pastor Alemão':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/pastoralemao.png',
+  'Lulu da Pomerânia':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/luludapomerania.png',
+  'Husky Siberiano':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/huskysiberiano.png',
+  'Labrador Retriever':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/goldenretriever.png',
+  Beagle:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/beagle.png',
+  Poodle:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/Poodle.png',
+  Rottweiler:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/rottweiler.png',
+  'Shih Tzu':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/ShihTzu.png',
+
+  Labrador:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/labrador.png',
+
+  'Border Collie':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/borddercollie.png',
+
+  Rafeiro:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/borddercollie.png',
 };
 
 export const defaultImageUrl =
