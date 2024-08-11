25a5289a5f70fb089c127a9aba238ddd1d79063e
brunomachadors <brunomachadors@gmail.com>
2024-08-11 19:17:14 +0100

    Fix Images

--- src/app/assets/imageLinks.ts
+++ src/app/assets/imageLinks.ts
@@ -12,7 +12,7 @@ export const breedImageMap: Record<string, string> = {
   'Lulu da Pomerânia':
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/luludapomerania.png',
   'Husky Siberiano':
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/huskysiberiano.png',
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/huskysiberiano2.png',
   'Labrador Retriever':
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/goldenretriever.png',
   Beagle:
@@ -23,13 +23,10 @@ export const breedImageMap: Record<string, string> = {
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/rottweiler.png',
   'Shih Tzu':
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/ShihTzu.png',
-
   Labrador:
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/labrador.png',
-
   'Border Collie':
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/borddercollie.png',
-
   Rafeiro:
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/borddercollie.png',
 };
