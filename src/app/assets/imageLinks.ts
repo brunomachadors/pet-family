0b5ab93b3fb68a009018d36259341527649eb827
brunomachadors <brunomachadors@gmail.com>
2024-08-16 19:07:40 +0100

    Update image links

--- src/app/assets/imageLinks.ts
+++ src/app/assets/imageLinks.ts
@@ -1,36 +1,38 @@
 export const breedImageMap: Record<string, string> = {
+  Beagle:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/beagle.png',
+  'Border Collie':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/borddercollie.png',
+  Boxer:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723400397/PET%20FAMILTY/boxer.png',
   'Bulldog Francês':
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723115596/PET%20FAMILTY/french-bulldog_jbihqs.png',
+  'Golden Retriever':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/goldenretriever.png',
+  'Husky Siberiano':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/huskysiberiano2.png',
+  Labrador:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/labrador.png',
+  'Labrador Retriever':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/goldenretriever.png',
+  'Lulu da Pomerânia':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/luludapomerania.png',
+  'Pastor Alemão':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/pastoralemao.png',
+  Poodle:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/Poodle.png',
+  Rafeiro:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/borddercollie.png',
+  Rottweiler:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/rottweiler.png',
+  Schnauzer:
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723825420/PET%20FAMILTY/schnauzer.png',
+  'Shih Tzu':
+    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/ShihTzu.png',
   Siamês:
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723115596/PET%20FAMILTY/siamese_gpagdt.png',
   Viralata:
     'https://res.cloudinary.com/dtglidvcw/image/upload/v1723218491/PET%20FAMILTY/viralata.png',
-  'Golden Retriever':
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/goldenretriever.png',
-  'Pastor Alemão':
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/pastoralemao.png',
-  'Lulu da Pomerânia':
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/luludapomerania.png',
-  'Husky Siberiano':
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/huskysiberiano2.png',
-  'Labrador Retriever':
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723393677/PET%20FAMILTY/goldenretriever.png',
-  Beagle:
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/beagle.png',
-  Poodle:
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/Poodle.png',
-  Rottweiler:
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/rottweiler.png',
-  'Shih Tzu':
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723394094/PET%20FAMILTY/ShihTzu.png',
-  Labrador:
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/labrador.png',
-  'Border Collie':
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/borddercollie.png',
-  Rafeiro:
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723398758/PET%20FAMILTY/borddercollie.png',
-  Boxer:
-    'https://res.cloudinary.com/dtglidvcw/image/upload/v1723400397/PET%20FAMILTY/boxer.png',
 };
 
 export const defaultImageUrl =
