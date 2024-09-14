7a280435e878f1fb8254fcf43695440043a374be
brunomachadors <brunomachadors@gmail.com>
2024-09-14 23:33:47 +0100

    Fix banner image

--- src/app/components/DogBanner/style.tsx
+++ src/app/components/DogBanner/style.tsx
@@ -2,28 +2,30 @@ import styled from 'styled-components';
 
 export const BannerDiv = styled.div`
   background-color: #f5f7f8;
-  width: 100vw;
-  height: 500px;
+  height: 400px;
   display: flex;
   justify-content: center;
   align-items: center;
   overflow: hidden;
-
-  @media (min-width: 1281px) {
-    max-width: 1200px;
-    margin: 0 auto;
-  }
+  width: 100%;
+  position: relative;
 `;
 
 export const ImageWrapper = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: relative;
-  img {
-    object-fit: cover;
 
-    @media (min-width: 1281px) {
-      object-fit: contain;
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: contain;
+
+    @media (max-width: 768px) {
+      object-fit: cover;
     }
   }
 `;
